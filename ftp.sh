2dd7cebe02e17854535c1313e429ff1833be4285
Charlie <charles.jiangbl@gmail.com>
2014-12-05 21:22:25 +1300

    updated the ftp script to allow specify the path with or without the slash

--- ftp.sh
+++ ftp.sh
@@ -3,13 +3,26 @@
 function list()
 {
   local path="$1";
-  ncftpls -m "$path" | while read a; do
-    if [ ${a: -1} == "/" ]; then
-      list "${path}${a}";
-    else
-      echo "${path}${a}";
-    fi
-  done;
+  p=$(ncftpls -m "$path");
+  # turn p into array of file paths for the given $path
+  p=( $p )
+  # if the given $path is for a single file then print it
+  if [[ ${#p[@]} -eq 1 && ${p[@]} == ${path: $(( -1 * ${#p[0]} ))} ]]; then
+    echo "${path}";
+  else
+    # otherwise for each file path
+    for a in ${p[@]}; do
+      # extract the last part of the path, the listed in p contains the dir name
+      i=$(echo $a | sed -r 's|^.*/([^/]+)/?$|\1|')
+      # if path is a dir, list it
+      if [ ${a: -1} == "/" ]; then
+        list "${path}/${i}";
+      else
+        # otherwise print it 
+        echo "${path}/${i}";
+      fi
+    done
+  fi
 }
 
 list "$1";
